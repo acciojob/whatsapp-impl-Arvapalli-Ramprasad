e3ef64e4345af3f5fac00cb8db0857f0e88d54fe
Ramprasad <ramprasad6352@gmail.com>
2024-06-01 20:44:36 +0530

    User addes

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -24,25 +24,25 @@ public class WhatsappService {
         return whatsappRepository.createGroup(users);
     }
 
-    public int createMessage(String content){
-        // The 'i^th' created message has message id 'i'.
-        return whatsappRepository.createMessage(content);
-    }
-
-    public int sendMessage(Message message, User sender, Group group) throws Exception{
-        //Throw "Group does not exist" if the mentioned group does not exist
-        //Throw "You are not allowed to send message" if the sender is not a member of the group
-
-        return whatsappRepository.sendMessage(message,sender,group);
-    }
-    //
-    public String changeAdmin(User approver, User user, Group group) throws Exception{
-        //Change the admin of the group to "user".
-        //Throw "Group does not exist" if the mentioned group does not exist
-        //Throw "Approver does not have rights" if the approver is not the current admin of the group
-        //Throw "User is not a participant" if the user is not a part of the group
-        return "SUCCESS";
-    }
+//    public int createMessage(String content){
+//        // The 'i^th' created message has message id 'i'.
+//        return whatsappRepository.createMessage(content);
+//    }
+//
+//    public int sendMessage(Message message, User sender, Group group) throws Exception{
+//        //Throw "Group does not exist" if the mentioned group does not exist
+//        //Throw "You are not allowed to send message" if the sender is not a member of the group
+//
+//        return whatsappRepository.sendMessage(message,sender,group);
+//    }
+//    //
+//    public String changeAdmin(User approver, User user, Group group) throws Exception{
+//        //Change the admin of the group to "user".
+//        //Throw "Group does not exist" if the mentioned group does not exist
+//        //Throw "Approver does not have rights" if the approver is not the current admin of the group
+//        //Throw "User is not a participant" if the user is not a part of the group
+//        return "SUCCESS";
+//    }
 //    /ujwehuha
 //
 //    public int removeUser(User user) throws Exception{
